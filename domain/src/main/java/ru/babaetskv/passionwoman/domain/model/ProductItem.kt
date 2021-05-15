a7b1706a33e91c0e735cc7ef99fa7c8818a1ba22
lider123 <k_babaec@mail.ru>
2021-05-15 11:35:42 +0500

    Removed redundant files

--- domain/src/main/java/ru/babaetskv/passionwoman/domain/model/ProductItem.kt
+++
@@ -1,8 +0,0 @@
-package ru.babaetskv.passionwoman.domain.model
-
-data class ProductItem(
-    val color: ProductColor,
-    val images: List<Image>,
-    val price: Float,
-    val rating: Float
-)
