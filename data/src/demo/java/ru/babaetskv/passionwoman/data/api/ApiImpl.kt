0ac2402cefe203effbd8ba9ca4b48ad1ddd4b037
lider123 <k_babaec@mail.ru>
2021-05-17 09:41:11 +0500

    Added new assets

--- data/src/demo/java/ru/babaetskv/passionwoman/data/api/ApiImpl.kt
+++ data/src/demo/java/ru/babaetskv/passionwoman/data/api/ApiImpl.kt
@@ -20,7 +20,7 @@ class ApiImpl(
             .build()
 
     override suspend fun getCategories(): List<CategoryModel> = withContext(Dispatchers.IO) {
-        delay(500L)
+        delay(DELAY_LOADING)
         val json = assetManager.open("categories.json").bufferedReader().use{ it.readText()}
         val listType = Types.newParameterizedType(List::class.java, CategoryModel::class.java)
         val adapter: JsonAdapter<List<CategoryModel>> = moshi.adapter(listType)
@@ -28,20 +28,33 @@ class ApiImpl(
     }
 
     override suspend fun getProducts(categoryId: String): List<ProductModel> = withContext(Dispatchers.IO) {
+        delay(DELAY_LOADING)
         val filename = CategoryProducts.findByCategoryId(categoryId)!!.productsFileName
         val json = assetManager.open(filename).bufferedReader().use{ it.readText()}
         val listType = Types.newParameterizedType(List::class.java, ProductModel::class.java)
         val adapter: JsonAdapter<List<ProductModel>> = moshi.adapter(listType)
-        return@withContext (adapter.fromJson(json) ?: emptyList())
+        return@withContext adapter.fromJson(json) ?: emptyList()
     }
 
     enum class CategoryProducts(val categoryId: String, val productsFileName: String) {
         BRA("category_bra", "products_bra.json"),
-        PANTIES("category_panties", "products_panties.json");
+        PANTIES("category_panties", "products_panties.json"),
+        LINGERIE("category_lingerie", "products_lingerie.json"),
+        EROTIC("category_erotic", "products_erotic.json"),
+        SWIM("category_swim", "products_swim.json"),
+        CORSET("category_corsets", "products_corset.json"),
+        GARTERBELT("category_garter_belts", "products_garter_belts.json"),
+        BABYDOLL("category_babydolls", "products_babydoll.json"),
+        STOCKINGS("category_stockings", "products_stockings.json"),
+        PANTYHOSE("category_pantyhose", "products_pantyhose.json");
 
         companion object {
 
             fun findByCategoryId(id: String) = values().find { it.categoryId == id }
         }
+    }
+
+    companion object {
+        private const val DELAY_LOADING = 500L
     }
 }
