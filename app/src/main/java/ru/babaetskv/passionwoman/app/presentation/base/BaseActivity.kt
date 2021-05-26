2268c391f0d64ccb13259a8622bd475a7d7f4e4c
lider123 <k_babaec@mail.ru>
2021-05-26 16:42:02 +0500

    Added keyboard animations and fixed sign up design

--- app/src/main/java/ru/babaetskv/passionwoman/app/presentation/base/BaseActivity.kt
+++ app/src/main/java/ru/babaetskv/passionwoman/app/presentation/base/BaseActivity.kt
@@ -5,9 +5,23 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.github.terrakok.cicerone.Router
 import org.koin.android.ext.android.inject
+import ru.babaetskv.passionwoman.app.presentation.SimpleKeyboardAnimator
 
 abstract class BaseActivity : AppCompatActivity() {
+    private val keyboardAnimator: SimpleKeyboardAnimator by lazy {
+        SimpleKeyboardAnimator(window)
+    }
     protected val router: Router by inject()
+
+    override fun onStart() {
+        super.onStart()
+        keyboardAnimator.start()
+    }
+
+    override fun onStop() {
+        keyboardAnimator.stop()
+        super.onStop()
+    }
 
     protected fun hideKeyboard() {
         val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
