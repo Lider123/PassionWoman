926e489ca7032910055d52dce2eddb54e57b08eb
lider123 <k_babaec@mail.ru>
2021-07-26 12:45:03 +0500

    Added adapter delagates for demo presets

--- app/src/demo/java/ru/babaetskv/passionwoman/app/presentation/feature/demopresets/DemoPresetsActivity.kt
+++ app/src/demo/java/ru/babaetskv/passionwoman/app/presentation/feature/demopresets/DemoPresetsActivity.kt
@@ -2,6 +2,7 @@ package ru.babaetskv.passionwoman.app.presentation.feature.demopresets
 
 import android.content.Intent
 import android.viewbinding.library.activity.viewBinding
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.babaetskv.passionwoman.app.R
 import ru.babaetskv.passionwoman.app.databinding.ActivityDemoPresetsBinding
@@ -11,8 +12,11 @@ import ru.babaetskv.passionwoman.app.presentation.base.BaseActivity
 
 class DemoPresetsActivity : BaseActivity<DemoPresetsViewModel, DemoPresetsViewModel.Router>() {
     private val binding: ActivityDemoPresetsBinding by viewBinding()
-    private val adapter: DemoPresetsAdapter by lazy {
-        DemoPresetsAdapter(viewModel::onPresetChanged)
+    private val adapter: ListDelegationAdapter<List<DemoPreset>> by lazy {
+        ListDelegationAdapter(
+            singleDemoPresetDelegate(viewModel::onPresetChanged),
+            multiDemoPresetDelegate(viewModel::onPresetChanged)
+        )
     }
 
     override val contentViewRes: Int = R.layout.activity_demo_presets
@@ -48,6 +52,9 @@ class DemoPresetsActivity : BaseActivity<DemoPresetsViewModel, DemoPresetsViewMo
     }
 
     private fun populatePresets(presets: List<DemoPreset>) {
-        adapter.submitList(presets)
+        adapter.run {
+            items = presets
+            notifyDataSetChanged()
+        }
     }
 }
