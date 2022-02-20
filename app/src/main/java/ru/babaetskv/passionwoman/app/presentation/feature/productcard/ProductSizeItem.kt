275a880e485a239b1c8cd484cd262e890be5ce2d
lider123 <k_babaec@mail.ru>
2022-02-20 21:07:54 +0100

    Removed deprecated classess

--- app/src/main/java/ru/babaetskv/passionwoman/app/presentation/feature/productcard/ProductSizeItem.kt
+++
@@ -1,9 +0,0 @@
-package ru.babaetskv.passionwoman.app.presentation.feature.productcard
-
-import ru.babaetskv.passionwoman.domain.model.ProductSize
-
-@Deprecated("Use SelectableItem instead")
-data class ProductSizeItem(
-    val size: ProductSize,
-    val isSelected: Boolean = false
-)
--- app/src/main/java/ru/babaetskv/passionwoman/app/presentation/feature/productlist/sorting/SortingItem.kt
+++
@@ -1,9 +0,0 @@
-package ru.babaetskv.passionwoman.app.presentation.feature.productlist.sorting
-
-import ru.babaetskv.passionwoman.domain.model.Sorting
-
-@Deprecated("Use SelectableItem instead")
-data class SortingItem(
-    val sorting: Sorting,
-    val selected: Boolean
-)
