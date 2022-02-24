8cc3eb61e5d61fed36db215f8622eb292a60b52d
Babaets <Konstantin.Babaets@excelsior-solutions.de>
2022-02-24 10:22:18 +0100

    Refactored paged response using delegates

--- domain/src/main/java/ru/babaetskv/passionwoman/domain/model/ProductsPagedResponse.kt
+++ domain/src/main/java/ru/babaetskv/passionwoman/domain/model/ProductsPagedResponse.kt
@@ -3,8 +3,8 @@ package ru.babaetskv.passionwoman.domain.model
 import ru.babaetskv.passionwoman.domain.model.base.PagedResponse
 import ru.babaetskv.passionwoman.domain.model.filters.Filter
 
-data class ProductsPagedResponse(
-    override val items: List<Product>,
+class ProductsPagedResponse(
+    items: List<Product>,
     override val total: Int,
     val availableFilters: List<Filter>
-) : PagedResponse<Product>()
+) : PagedResponse<Product>(items)
--- domain/src/main/java/ru/babaetskv/passionwoman/domain/model/base/PagedResponse.kt
+++ domain/src/main/java/ru/babaetskv/passionwoman/domain/model/base/PagedResponse.kt
@@ -1,29 +1,5 @@
 package ru.babaetskv.passionwoman.domain.model.base
 
-abstract class PagedResponse<T> : List<T> {
-    abstract val items: List<T>
+abstract class PagedResponse<T>(items: List<T>) : List<T> by items {
     abstract val total: Int
-
-    override val size: Int
-        get() = items.size
-
-    override fun contains(element: T): Boolean = items.contains(element)
-
-    override fun containsAll(elements: Collection<T>): Boolean = items.containsAll(elements)
-
-    override fun get(index: Int): T = items[index]
-
-    override fun indexOf(element: T): Int = items.indexOf(element)
-
-    override fun isEmpty(): Boolean = items.isEmpty()
-
-    override fun iterator(): Iterator<T> = items.iterator()
-
-    override fun lastIndexOf(element: T): Int = items.lastIndexOf(element)
-
-    override fun listIterator(): ListIterator<T> = items.listIterator()
-
-    override fun listIterator(index: Int): ListIterator<T> = items.listIterator(index)
-
-    override fun subList(fromIndex: Int, toIndex: Int): List<T> = items.subList(fromIndex, toIndex)
 }
